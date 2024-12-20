f99e9ebe636e8d6429ba7fc1ee582266e0ef3451
awariel7 <awariel1@gmail.com>
2024-12-20 15:28:04 +0300

    - Fix answer check for task2

--- Rounds/RAM_Restore.py
+++ Rounds/RAM_Restore.py
@@ -2,11 +2,13 @@ Moroz_pass = 'noviy_god_2025'
 
 RAM_list = ['0x' + str(i) for i in range(1, len(Moroz_pass) + 1)]
 RAM_data = []
+Skipped_data = set()
 
 for s in Moroz_pass:
     info = ''
     if s in "oa0g":
         RAM_data.append('ERROR')
+        Skipped_data.add(str(ord(s)))
     else:
         RAM_data.append(str(ord(s)))
 address = ' '.join(RAM_list)
@@ -27,7 +29,7 @@ Cистема загрузилась! Так...<br>
 О, нет, обнаружены повреждения в оперативной памяти! Некоторое количество блоков данных утрачено, а значит и пароль к моей базе!<br>
 Всё что осталось в ячейках памяти - ASCII коды символов пароля, да и то не все!<br>
 Помогите мне восстановить пароль, переведите недостающие символы в ASCII код и <b>пришлите мне их ASCII-коды через пробел в порядке появления в пароле</b>.<br>
-Если один символ упоминается несколько раз, то вы должны прислать мне его <b>только один раз</b>.<br>
+Если один символ упоминается несколько раз, то вы должны прислать мне его <b>только один раз</b> без лидирующих нулей.<br>
 А я пока проверю целостность своего жёсткого диска..<br>
 <br>
 Адреса памяти с оставшимися данными:"""
@@ -35,7 +37,7 @@ Cистема загрузилась! Так...<br>
 error_message = 'Не подходит...Есть другие варианты?'
 success_message = 'Подходит!'
 button_text = 'Восстановим бэкап базы'
-
+# print(' '.join(list(Skipped_data)))
 def check_answer(answer):
-    correct_input = '111 103 48'
+    correct_input = ' '.join(list(Skipped_data))
     return answer.strip().lower() == correct_input
